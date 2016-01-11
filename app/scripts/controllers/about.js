61e67b3e22e5a3f9db54b228f1a60157c5d51f2d
Hòa Nguyễn Văn <nvh0412@gmail.com>
2016-01-11 23:43:40 +0700

    [c] initialize Front-end Application with Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abraFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the abraFeApp
+ */
+angular.module('abraFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abraFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the abraFeApp
+ */
+angular.module('abraFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
